3905f5adf2ef44f26707e53ce617cc5e487e7f2b
BencoLee <alexmao930918@gmail.com>
2017-10-18 14:06:41 +0800

    rename test_jar to test_api is property

--- flask_schema/tests/contrib/test_base.py
+++ flask_schema/tests/contrib/test_base.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import pytest
+from flask import Flask
+
+from flask_schema.contrib.base import FlaskAPIClass, Jar
+
+
+flask_app = Flask(__name__)
+
+
+def test_singleton_jar():
+    jar1 = Jar(flask_app, "jar1")
+    jar2 = Jar(flask_app, "jar2")
+
+    assert jar1 == jar2
+    assert jar1 is jar2
+    assert id(jar1) == id(jar2)
