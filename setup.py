7f8e1ff3fd163d2820831ab7b36065705cd8b1e6
BencoLee <alexmao930918@gmail.com>
2017-10-19 18:17:21 +0800

    Remove demo to flask_schema root dir and refine setup file

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ import os
 from setuptools import setup, find_packages
 
 
+FLASK_SCHEMA_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    "flask_schema"
+)
+
+
 setup(
     name="flask_schema",
     version="0.0.1",
-    packages=find_packages(
-        os.path.abspath(os.path.dirname(__file__))),
+    packages=find_packages(FLASK_SCHEMA_DIR),
     url="https://github.com/BencoLee/flask_schema",
     description="Flask Schema Tools",
 )
